de9cc0a98a3e65d1bd8e98438f8de04d060ca069
unknown <caianlopesfonseca@gmail.com>
2025-09-29 22:27:34 -0300

    Criado controller

---
+++ src/main/java/com/example/poo/Controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.poo.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UserController {
+
+}
